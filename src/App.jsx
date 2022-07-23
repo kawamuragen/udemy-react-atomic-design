75ca77cd9497211dc40dc189611df12b3b9c29fa
Gen Kawamura <64403862+kawamuragen@users.noreply.github.com>
2022-07-23 13:03:14 +0900

    Up to lesson 32.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
 
 export const App = () => {
   return (
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
-      <PrimaryButton>検索</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
+      <br />
+      <SearchInput />
+      <UserCard />
     </div>
   );
 };
