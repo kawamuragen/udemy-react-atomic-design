4e30995bdbb0fd52aa5662fdfdf20d5c6e98b301
Gen Kawamura <64403862+kawamuragen@users.noreply.github.com>
2022-07-24 12:01:57 +0900

    Up to lesson 36.

---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+// サンプルユーザオブジェクト
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `じゃけー${val}`,
+    image: "https://source.unsplash.com/NE0XGVKTmcA",
+    email: "12345@example.com",
+    phone: "090-1111-2222",
+    company: {
+      name: "テスト株式会社"
+    },
+    website: "https://www.google.com"
+  };
+});
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>ユーザー一覧</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  text-align: center;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  grid-gap: 20px;
+`;
